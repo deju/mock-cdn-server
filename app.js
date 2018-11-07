0c10e8b715411b45f8d54aad6919503ff0c5e341
deju <deju.tu@gmail.com>
2018-11-08 00:40:50 +0800

    feat(listfile): support listfile

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const readConfig = require('read-config');
 const path = require('path');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
+const serveIndex = require('serve-index');
 const mkdir = require('./mkdir');
 const codes = require('./codes');
 
-const config = readConfig('./config');
+let config = readConfig('./config');
+
+const dirname = __dirname;
+if (fs.existsSync(path.join(dirname, 'local.config.json'))) {
+    config = readConfig('./local.config.json');
+}
+
 const staticPath = config.cdn_path || '';
-const staticDir = path.join(__dirname, config.file_dir);
+const staticDir = path.join(dirname, config.file_dir);
 
 const app = express();
 if (!fs.existsSync(staticDir)) {
@@ -17,6 +24,7 @@ if (!fs.existsSync(staticDir)) {
 }
 
 app.use(express.static(staticDir));
+app.use('/files', serveIndex(staticDir, { 'icons': true }));
 
 app.use(fileUpload());
 
@@ -34,7 +42,9 @@ app.post('/upload', function (req, res) {
                 mkdir(path.dirname(curFilePath));
             }
             if (fs.existsSync(curFilePath)) {
-                throw new Error(curFilePath.replace(staticDir, '') + ' exists already', codes.EXIST);
+                const err = new Error(curFilePath.replace(staticDir, '') + ' exists already');
+                err.code = codes.EXIST;
+                throw err;
             }
 
             fs.writeFileSync(curFilePath, curFile.data, 'utf-8');
@@ -46,6 +56,12 @@ app.post('/upload', function (req, res) {
             message: 'success'
         });
     } catch (e) {
+        if (e && e.code === codes.EXIST) {
+            return res.status(405).json({
+                code: codes.EXIST,
+                message: e.message
+            });
+        }
         return res.status(409).json({
             code: codes.ERROR,
             message: e.message
@@ -54,7 +70,8 @@ app.post('/upload', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-    res.send('simple-cdn-server doing');
+    res.send('<br>simple-cdn-server doing, all files listing in <a href="/files/">here</a>');
 })
 
 app.listen(3030);
+console.log('listen 3030');
